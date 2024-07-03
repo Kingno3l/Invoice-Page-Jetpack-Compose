4b2c8f8b6ef62c924ef979d385af8e86ebbd8a50
kingno3l <nuelluiz@gmail.com>
2024-07-03 16:15:23 +0100

    item arranged on the table properly

--- app/src/main/java/android/example/newcomposetable/MainActivity.kt
+++ app/src/main/java/android/example/newcomposetable/MainActivity.kt
@@ -12,11 +12,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.example.newcomposetable.ui.theme.NewComposeTableTheme
+import android.provider.Telephony.Mms.Rate
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import com.breens.beetablescompose.BeeTablesCompose
+
+data class User(
+    val numb: Int,
+    val item_desc: String,
+    val Qty: Double,
+    val Rate: Double,
+    val Amount: Double
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +38,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val users = listOf(
-                User(name =  "John", age  =  25, email =  "helloemail@example.com"),
-                User(name =  "Sim", age  =  15, email =  "simmers@example.com"),
-                User(name =  "Sam", age  =  12, email =  "samore@example.com"),
-                User(name =  "Ade", age  =  56, email =  "adele@example.com"),
+                User(numb =  1, item_desc =  "Pepe Jeans", Qty =  1.00, Rate = 24.99, Amount = 24.99),
+                User(numb =  2, item_desc =  "Boys Shirts", Qty =  4.00, Rate = 52.99, Amount = 52.99),
+                User(numb =  3, item_desc =  "Girls Shirts", Qty =  3.00, Rate = 41.99, Amount = 41.99),
             )
-            val tableHeaders = listOf("Name", "Age", "Email")
+
+            val tableHeaders = listOf("#", "Item & Description", "Qty", "Rate", "Amount")
             NewComposeTableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
@@ -39,7 +53,7 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize(),
                         contentAlignment = Alignment.Center
                     ) {
-                        BeeTablesCompose(data = users, headerTableTitles = tableHeaders)
+                        MainContent(users = users, headers = tableHeaders)
                     }
                 }
             }
@@ -47,9 +61,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class User(
-    val name: String,
-    val age: Int,
-    val email: String
-)
+@Composable
+fun MainContent(users: List<User>, headers: List<String>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            ItemTable(users = users, headers = headers)
+        }
+        item {
+            ItemTable(users = users, headers = headers)
+        }
+    }
+}
+
+@Composable
+fun ItemTable(users: List<User>, headers: List<String>) {
+    BeeTablesCompose(data = users, headerTableTitles =headers, columnToIndexIncreaseWidth = 1)
+}
 
