1e7009b3f4709bf43e89bec027389a1a0a250032
kingno3l <nuelluiz@gmail.com>
2024-07-04 13:41:14 +0100

    working now, now to add the summary

--- app/src/main/java/android/example/newcomposetable/MainActivity.kt
+++ app/src/main/java/android/example/newcomposetable/MainActivity.kt
@@ -21,8 +21,30 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.breens.beetablescompose.BeeTablesCompose
+import android.example.newcomposetable.ui.theme.Green12
+import android.media.tv.TvContract.Channels.Logo
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+
 
 data class User(
     val numb: Int,
@@ -30,6 +52,12 @@ data class User(
     val Qty: Double,
     val Rate: Double,
     val Amount: Double
+)
+
+data class Invoice(
+    val invoice_date: String,
+    val terms: String,
+    val due_date: String
 )
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +71,14 @@ class MainActivity : ComponentActivity() {
                 User(numb =  3, item_desc =  "Girls Shirts", Qty =  3.00, Rate = 41.99, Amount = 41.99),
             )
 
+            val invoiceInfo = listOf(
+                Invoice(invoice_date = "18 July 2024", terms = "Due on Receipt", due_date = "18 August 2024")
+            )
+
             val tableHeaders = listOf("#", "Item & Description", "Qty", "Rate", "Amount")
+
+            val invoiceHeaders = listOf("Invoice Date", "Terms", "Due Date")
+
             NewComposeTableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
@@ -53,7 +88,12 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize(),
                         contentAlignment = Alignment.Center
                     ) {
-                        MainContent(users = users, headers = tableHeaders)
+                        MainContent(
+                            users = users,
+                            invoiceInfo = invoiceInfo,
+                            headers = tableHeaders,
+                            invoiceHeaders = invoiceHeaders
+                        )
                     }
                 }
             }
@@ -62,22 +102,147 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent(users: List<User>, headers: List<String>) {
+fun MainContent(users: List<User>, invoiceInfo: List<Invoice>, headers: List<String>, invoiceHeaders: List<String>) {
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        item { Logo() }
+
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
+
+        item { AddressSection() }
+
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
+
+        item {
+            InvoiceTable(invoiceInfo = invoiceInfo, headers = invoiceHeaders)
+        }
+
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
         item {
             ItemTable(users = users, headers = headers)
         }
-        item {
-            ItemTable(users = users, headers = headers)
+
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
+        item { BillSummary() }
+
+        item { Spacer(modifier = Modifier.height(16.dp)) }
+
+
+        item { DownloadInvoiceButton() }
+
+
+
+
+    }
+}
+
+@Composable
+fun InvoiceTable(invoiceInfo: List<Invoice>, headers: List<String>) {
+    BeeTablesCompose(
+        data = invoiceInfo,
+        headerTableTitles = headers,
+        headerTitlesBackGroundColor = Green12,
+        headerTitlesTextStyle = TextStyle(Color.White, fontWeight = FontWeight.Bold)
+    )
+}
+
+
+@Composable
+fun ItemTable(users: List<User>, headers: List<String>) {
+    BeeTablesCompose(
+        data = users,
+        headerTableTitles = headers,
+        columnToIndexIncreaseWidth = 1,
+        headerTitlesBackGroundColor = Green12,
+        tableRowColors = listOf(Color.White, Color.LightGray),
+        headerTitlesTextStyle = TextStyle(Color.White, fontWeight = FontWeight.Bold)
+    )
+}
+
+@Composable
+fun Logo() {
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.End)
+    {
+
+        Image(
+            painter = painterResource(id = R.drawable.logodpx),
+            contentDescription = "Company Logo",
+            modifier = Modifier.size(48.dp) // Adjust size as needed
+        )
+    }
+}
+
+@Composable
+fun AddressSection() {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+            Text(text = "Ship To", style = MaterialTheme.typography.titleMedium)
+            Text(text = "2476 Blackwell Street")
+            Text(text = "Fairbanks")
+            Text(text = "99701 Colorado")
+            Text(text = "U.S.A")
         }
     }
 }
 
 @Composable
-fun ItemTable(users: List<User>, headers: List<String>) {
-    BeeTablesCompose(data = users, headerTableTitles =headers, columnToIndexIncreaseWidth = 1)
+fun BillSummary(modifier: Modifier = Modifier, billAmount: Float = 61.97f, taxRate: Float = 5.0f) {
+    Column(modifier) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Sub Total", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "$" + billAmount.toString(),
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Tax Rate", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = taxRate.toString() + "%", style = MaterialTheme.typography.headlineSmall)
+        }
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Total", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "$" + (billAmount + (billAmount * taxRate) / 100).toString(),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun DownloadInvoiceButton() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {  },
+            modifier = Modifier.padding(horizontal = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Green12,
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "Download Invoice")
+            Icon(painter = painterResource(id = R.drawable.baseline_file_download_24), contentDescription = "Download Icon")
+        }
+    }
 }
 
